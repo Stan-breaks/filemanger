9ea9102be2ce4c884f34cd5ceb5dbd1d911d44ab
Stan-breaks <stanleymwendwa03@gmail.com>
2025-04-25 08:20:58 +0300

    Display trait
    to use use with println! macro

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,35 @@
+#![allow(dead_code)]
+
+use std::fmt;
+use std::fmt::Display;
+
 #[derive(Debug, PartialEq)]
 enum FileState {
     Open,
     Closed,
 }
+
 #[derive(Debug)]
 struct File {
     name: String,
     data: Vec<u8>,
     state: FileState,
 }
+
+impl Display for FileState {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            FileState::Open => write!(f, "OPEN"),
+            FileState::Closed => write!(f, "CLOSED"),
+        }
+    }
+}
+impl Display for File {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "<{} ({})>", self.name, self.state)
+    }
+}
+
 impl File {
     fn new(name: &str) -> File {
         File {
@@ -17,37 +38,10 @@ impl File {
             state: FileState::Closed,
         }
     }
-    fn read(self: &File, save_to: &mut Vec<u8>) -> Result<usize, String> {
-        if self.state != FileState::Open {
-            return Err(String::from("File must be open for reading"));
-        }
-        let mut tmp = self.data.clone();
-        let read_length = tmp.len();
-        save_to.reserve(read_length);
-        save_to.append(&mut tmp);
-        Ok(read_length)
-    }
 }
-fn open(mut f: File) -> Result<File, String> {
-    f.state = FileState::Open;
-    Ok(f)
-}
-fn close(mut f: File) -> Result<File, String> {
-    f.state = FileState::Closed;
-    Ok(f)
-}
-fn main() {
-    let mut f5 = File::new("5.txt");
-    let mut buffer: Vec<u8> = vec![];
-    if f5.read(&mut buffer).is_err() {
-        println!("Error checking is working");
-    }
-    f5 = open(f5).unwrap();
-    let f5_lenght = f5.read(&mut buffer).unwrap();
-    f5 = close(f5).unwrap();
 
-    let text = String::from_utf8_lossy(&buffer);
-    println!("{:?}", f5);
-    println!("{} is  {} bytes long", &f5.name, f5_lenght);
-    println!("{}", text);
+fn main() {
+    let f6 = File::new("6.txt");
+    println!("{:?}", f6);
+    println!("{}", f6);
 }
